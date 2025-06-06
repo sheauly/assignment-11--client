c3485c33311f9ccfeff8505d8adfc08cea2c09bc
halimawebd <inthesumon1010@gmail.com>
2025-06-06 19:56:34 +0600

    initial firebase project setup

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.apiKey,
+    authDomain: import.meta.env.authDomain,
+    projectId: import.meta.env.projectId,
+    storageBucket: import.meta.env.storageBucket,
+    messagingSenderId: import.meta.env.messagingSenderId,
+    appId: import.meta.env.appId
+};
+
+console.log * (firebaseConfig)
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
